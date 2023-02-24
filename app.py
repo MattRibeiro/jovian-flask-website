9351f97dbd4557e1229166c6aa55324ed6c6528b
Matt Ribeiro <matt.ribeiro@covenanteyes.com>
2023-02-24 13:22:57 +0000

    Added hardcoded dynamic data and api route for jobs.

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [{
+  "id": 1,
+  "title": "Data Scientist",
+  "salary": 100000,
+  "location": "New York City"
+}, {
+  "id": 2,
+  "title": "Machine Learning Engineer",
+  "salary": 120000,
+  "location": "San Francisco"
+}, {
+  "id": 3,
+  "title": "Artificial Intelligence Researcher",
+  "salary": 150000,
+  "location": "Seattle"
+}, {
+  "id": 4,
+  "title": "Computer Vision Engineer",
+  "salary": 110000,
+  "location": "Boston"
+}, {
+  "id": 5,
+  "title": "Natural Language Processing Scientist",
+  "salary": 130000,
+  "location": "Chicago"
+}]
+
+
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company='Jovian')
+
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
